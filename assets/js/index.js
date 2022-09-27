567ae38f6b4f999745aac7da20a05d68e5ef3943
adhyaksazhalifunnas <adhyaksazhalifunnas@gmail.com>
2022-09-27 16:21:56 +0700

    feat(back_end): start project

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+
+
+
+$("#input_data").submit(function(event){
+    alert("Data Inserted Successfully!");
+})
+
+$("#update_data").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url" : `http://localhost:3000/api/lpg/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully!");
+    })
+
+})
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url" : `http://localhost:3000/api/lpg/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload();
+            })
+        }
+
+    })
+}
---
+++ server/services/render.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+
+exports.homeRoutes = (req, res) => {
+    // Make a get request to /api/lpg
+    axios.get('http://localhost:3000/api/lpg')
+        .then(function(response){
+            res.render('index', { lpg : response.data });
+        })
+        .catch(err =>{
+            res.send(err);
+        })
+
+    
+}
+
+exports.input_data = (req, res) =>{
+    res.render('input_data');
+}
+
+exports.update_data = (req, res) =>{
+    axios.get('http://localhost:3000/api/lpg', { params : { id : req.query.id }})
+        .then(function(elpijidata){
+            res.render("update_data", { elpiji : elpijidata.data})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
+}
