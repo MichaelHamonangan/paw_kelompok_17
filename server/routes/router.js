68e5c5c1d38b1067a1824d7bf44e4b37654b82ac
M. Fadhil Mahendra <mfmahendr@gmail.com>
2022-09-28 19:31:40 +0700

    Add: router.js documentation

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,33 +1,57 @@
-const express = require('express');
-const route = express.Router()
+const express = require('express');                     // import express
+const route = express.Router()                          // create router
 
-const services = require('../services/render');
-const controller = require('../controller/controller');
+const services = require('../services/render');         // import services
+const controller = require('../controller/controller'); // import controller
 
+// -------------------- PAGE ROUTING --------------------
 /**
- *  @description Root Route
+ *  @description Routing for root page
  *  @method GET /
  */
 route.get('/', services.homeRoutes);
 
 /**
- *  @description add lpg
+ *  @description Routing for add new data page
  *  @method GET /input-data
  */
 route.get('/input-data', services.input_data)
 
 /**
- *  @description for update elpiji
+ *  @description Routing for update elpiji page
  *  @method GET /update-data
  */
 route.get('/update-data', services.update_data)
 
 
-// API
-route.post('/api/lpg', controller.create);
+// ------------------------------ API ROUTING ------------------------------
+/**
+ * @description find elpiji with or without id
+ * if id is not provided, it will return all elpiji
+ * @method GET /api/lpg
+ * @method GET /api/lpg?id={id}
+ */
 route.get('/api/lpg', controller.find);
+
+/**
+ * @description Create new elpiji
+ * @method POST /api/lpg
+ */
+route.post('/api/lpg', controller.create);
+
+/**
+ * @description Update specified elpiji with id
+ * @method POST /api/lpg
+ * @param req.params.id
+ */
 route.put('/api/lpg/:id', controller.update);
+
+/**
+ * @description Delete specified elpiji with id
+ * @method DELETE /api/lpg/:id
+ * @param req.params.id
+ */
 route.delete('/api/lpg/:id', controller.delete);
 
-
+// Export routes
 module.exports = route
