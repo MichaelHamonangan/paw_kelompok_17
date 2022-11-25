b7ae4a2d991abc8108898a6cdf02ce2955ca5efc
adhyaksazhalifunnas <adhyaksazhalifunnas@gmail.com>
2022-11-25 09:18:26 +0700

    feat(Delete): Working delete button
    + dummy error404 page

---
+++ frontend/src/pages/Error404.jsx
@@ -0,0 +1,14 @@
+function Error404() {
+    return (
+        <>
+        <section className='heading'>
+            <h1>
+            ERROR 404 : Page Not Found
+            </h1>
+            <p>Cahyo Sumber Migas</p>
+        </section>
+        </>
+    )
+}
+
+export default Error404
