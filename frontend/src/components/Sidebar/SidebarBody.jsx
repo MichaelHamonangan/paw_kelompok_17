29a4b33384e3f0c8df713ba405c0df31fc410f06
Amakoy <71454818+Amakoy@users.noreply.github.com>
2022-12-02 05:25:54 +0700

    feat(frontend): Sidebar and Login
    Added collapsible sidebar and Shadow box for the login and register page

--- frontend/src/components/Sidebar/SidebarBody.jsx
+++ frontend/src/components/Sidebar/SidebarBody.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import SidebarBodyItem from "./SidebarBodyItem";
 import { faHome, faHandHoldingDollar, faDashboard } from "@fortawesome/free-solid-svg-icons";
 
-function SidebarBody(){
+function SidebarBody({opene}){
     return (
         <nav>
             <SidebarBodyItem 
                 faIcon={faHome}
                 linkTo='/'
                 title='Home'
+                open={opene}
             />
             <SidebarBodyItem 
                 faIcon={faDashboard}
                 linkTo='/dashboard'
                 title='Dashboard'
+                open={opene}
             />
             <SidebarBodyItem 
                 faIcon={faHandHoldingDollar}
                 linkTo='/penjualan-lpg'
                 title='Penjualan'
+                open={opene}
             />
             {/* <SidebarBodyItem 
                 faIcon={faUser}
