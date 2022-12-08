34515136bda955eaebea604b6ec1afda18842806
M. Fadhil Mahendra <mfmahendr@gmail.com>
2022-12-08 20:32:24 +0700

    Update Responsive Web Design

--- frontend/src/pages/AddData/InputDataForm.jsx
+++ frontend/src/pages/AddData/InputDataForm.jsx
@@ -104,7 +104,7 @@ function InputDataForm(){
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="input-form__card">
             <div className="new_data">
                 <div className="form-group">
                     <label htmlFor="tanggal" className="text-light">Tanggal</label>
@@ -112,14 +112,16 @@ function InputDataForm(){
                     <input type="text" name="tanggal" value={tanggal} placeholder="Contoh = TTTT-BB-HH" onChange={onChange}/>
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="kode" className="text-light">Kode</label>
-                    <input type="text" name="kode" value={kode} placeholder="Dua huruf terakhir plat nomor kendaraan" onChange={onChange}/>
-                </div>
+                <div className="form-group-row">
+                    <div className="form-group">
+                        <label htmlFor="kode" className="text-light">Kode</label>
+                        <input type="text" name="kode" value={kode} placeholder="Dua huruf terakhir plat nomor kendaraan" onChange={onChange}/>
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="nama" className="text-light">Nama Pangkalan</label>
-                    <input type="text" name="nama" value={nama} placeholder="Contoh = Siswoyo" onChange={onChange}/>
+                    <div className="form-group">
+                        <label htmlFor="nama" className="text-light">Nama Pangkalan</label>
+                        <input type="text" name="nama" value={nama} placeholder="Contoh = Siswoyo" onChange={onChange}/>
+                    </div>
                 </div>
 
                 <div className="form-group">
@@ -127,16 +129,18 @@ function InputDataForm(){
                     <input type="text" name="keterangan" value={keterangan} placeholder="Keterangan" onChange={onChange}/>
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="Tabung" className="text-light">Jumlah Tabung</label>
-                    <input type="number" name="tabung_transfer" value={tabung_transfer} placeholder="Cash-less" onChange={(e) => countPriceCashLess(e)}/>
-                    <input type="number" name="tabung_tunai" value={tabung_tunai} placeholder="Tunai" onChange={(e) => countPriceTunai(e)}/>
-                </div>
+                <div className="form-group-row">
+                    <div className="form-group">
+                        <label htmlFor="Tabung" className="text-light">Jumlah Tabung</label>
+                        <input type="number" name="tabung_transfer" value={tabung_transfer} placeholder="Cash-less" onChange={(e) => countPriceCashLess(e)}/>
+                        <input type="number" name="tabung_tunai" value={tabung_tunai} placeholder="Tunai" onChange={(e) => countPriceTunai(e)}/>
+                    </div>
 
-                <div className="form-group">
-                    <label htmlFor="Pembayaran" className="text-light">Pembayaran</label>
-                    <input type="number" name="bayar_transfer" value={bayar_transfer} placeholder="Cash-less" onChange={onChange}/>
-                    <input type="number" name="bayar_tunai" value={bayar_tunai} placeholder="Tunai" onChange={onChange}/>
+                    <div className="form-group">
+                        <label htmlFor="Pembayaran" className="text-light">Pembayaran</label>
+                        <input type="number" name="bayar_transfer" value={bayar_transfer} placeholder="Cash-less" onChange={onChange}/>
+                        <input type="number" name="bayar_tunai" value={bayar_tunai} placeholder="Tunai" onChange={onChange}/>
+                    </div>
                 </div>
         
                 <div className="form-group">
